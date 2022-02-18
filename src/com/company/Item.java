c4b954421b1cb559d85121ede6dfb7ac50d15d1c
LTH4823 <peter4823zx@naver.com>
2022-02-19 00:11:19 +0900

    update Item and Restaurant

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class Item {
+    String name;
+    int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/com/company/Restaurant.java
+++ src/com/company/Restaurant.java
@@ -1,4 +1,31 @@
 package com.company;
+import java.util.Arrays;
 
 public class Restaurant  {
+    Item item01 = new Item("짜장면",6000);
+    Item item02 = new Item("간짜장",7500);
+    Item item03 = new Item("해물짬뽕",7000);
+    Item item04 = new Item("만두",5000);
+    Item item05 = new Item("볶음밥",7500);
+    Item[] items = new Item[5];
+
+    public Item[] itemSet(){
+        items[0]=item01;
+        items[1]=item02;
+        items[2]=item03;
+        items[3]=item04;
+        items[4]=item05;
+        return items;
+    }
+
+    public Restaurant() {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "Restaurant{" +
+                "items=" + Arrays.toString(items) +
+                '}';
+    }
 }
